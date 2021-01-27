d80064b3d2dbf07c257ea5d47f4c7b2d2fb8b2c0
Angelika Blackburn <esonyei@gmail.com>
2021-01-27 07:38:53 -0500

    correct routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route } from "react-router-dom";
 import WeaponGenWrapper from "./Components/WeaponGenWrapper";
 import Navbar from "./Components/Navbar/Navbar";
 import Campaign from "./Components/Campaign/Campaign";
+import About from "./Components/About/About";
+import Donate from "./Components/Donate/Donate";
+import Home from "./Components/Home/Home";
 import "./CSS/App.css";
 
 function App() {
@@ -11,10 +14,19 @@ function App() {
 			<Navbar />
 			{/* Routes */}
 			<Route exact path="/">
-				<WeaponGenWrapper />
+				<Home />
 			</Route>
 			<Route path="/campaign">
 				<Campaign />
+			</Route>
+			<Route path="/generators">
+				<WeaponGenWrapper />
+			</Route>
+			<Route path="/donate">
+				<Donate />
+			</Route>
+			<Route path="/about">
+				<About />
 			</Route>
 			<footer>Footer Links </footer>
 		</div>
--- src/Components/Campaign/Campaign.js
+++ src/Components/Campaign/Campaign.js
@@ -3,7 +3,7 @@ import React from "react";
 function Campaign() {
 	return (
 		<div>
-			<h1>Hello World.</h1>
+			<h1>Campaign.</h1>
 		</div>
 	);
 }
